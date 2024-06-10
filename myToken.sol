2e8772e5a10d0d114ea3b0599e0f7cb2819ccb12
vjmabraham <201911705@fit.edu.ph>
2024-06-10 13:18:42 +0800

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -22,9 +22,17 @@ contract MyToken {
     string public tokenAbbrv = "ABR";
     uint public totalSupply = 0;
     // mapping variable here
-
+    mapping(address => uint) public balances;
     // mint function
-
+    function mint (address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
     // burn function
-
+    function burn (address _address, uint _value) public {
+        if (balances[_address] >= _value){
+            totalSupply -= _value;
+       balances[_address] -= _value;
+        } 
+    }
 }
